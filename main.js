46eb205819ca154fc9c66c266edadb5596d3deb6
morshmuel28 <mor.shmuel28@gmail.com>
2024-01-29 20:25:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const pages = {
+    "home": "white",
+    "projects": "lightblue",
+    "about": "lightgreen"
+}
+
+const showpage = (page) => {
+    document.querySelector('h1').innerText = page
+    document.querySelector('#content').style.backgroundColor = pages[page]
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btnTheme").addEventListener("click", function () {
+        document.documentElement.classList.toggle("dark")
+        document.getElementById("btnTheme").innerText = document.documentElement.classList.contains("dark") ? "light" : "dark"
+    })
+    document.getElementById("btnMenu").addEventListener("click", function(){
+        document.getElementById("popupMenu").classList.toggle("hidden")
+    })
+})
+document.querySelectorAll("a").forEach(function(el){
+    el.addEventListener("click", function(){
+        showpage(el.innerText.toLowerCase())
+    })
+})
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./*.{html,js,ts}"],
+  darkMode: 'class', //or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
